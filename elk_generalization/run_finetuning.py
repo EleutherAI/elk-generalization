2eb9e773d81e41c1918ae37e300c703402656fd3
Alex Mallen <atmallen8@gmail.com>
2023-11-10 04:01:29 +0000

    cleanup

--- elk_generalization/run_finetuning.py
+++ elk_generalization/run_finetuning.py
@@ -1,8 +1,9 @@
 import os
+from hashlib import md5
 
-os.environ["PYTHONHASHSEED"] = "633"
-
-models = ["EleutherAI/pythia-410m", "EleutherAI/pythia-1b", "EleutherAI/pythia-2.8b", "mistralai/Mistral-7B-v0.1", "meta-llama/Llama-2-7b-hf"]
+template="mixture"
+perturb="0.5"
+models = ["meta-llama/Llama-2-7b-hf", "mistralai/Mistral-7B-v0.1", "EleutherAI/pythia-2.8b", "EleutherAI/pythia-1b", "EleutherAI/pythia-410m"]
 lora_modules = {"gptneox": [
                     "dense_h_to_4h",
                     "dense_4h_to_h",
@@ -19,9 +20,7 @@ lora_modules = {"gptneox": [
         }
 
 for model in models:
-    template="mixture"
-    perturb="0.5"
-    modules = " ".join(lora_modules["gpneox" if "pythia" in model else "llama"])
+    modules = " ".join(lora_modules["gptneox" if "pythia" in model else "llama"])
     command = f"python finetuning.py --model {model} " \
             "--output-dir ../custom-models " \
             "--eval-every 50 --epochs 100 --fp16 " \
@@ -29,7 +28,7 @@ for model in models:
             "--grad-accumulation-steps 4 --lora-rank 8 " \
             f"--lora-modules {modules} " \
             f"--dataset \"atmallen/qm_{template}_1.0e_{perturb}p_finetuning\" "
-    id = str(hash(str(command)))[-8:]
+    id = md5(command.encode()).hexdigest()[-8:]
     model_last = model.split("/")[-1]
     command += f"--name \"{model_last}-{template}_{id}\""
     
