fix bug computing multitoken logprobs

elk_generalization/datasets/quirky_dataset.py
@@ -154,7 +154,8 @@ class QuirkyDataset(ABC):
             outputs = model(
                 torch.as_tensor([prompt], device=model.device), use_cache=True
             )
-            logits = outputs.logits[0, -1, [choice_toks[0][0], choice_toks[1][0]]]
+            logprob_output = outputs.logits[0, -1, :].log_softmax(dim=-1)
+            logprobs = logprob_output[[choice_toks[0][0], choice_toks[1][0]]]
 
             # we compute log_odds of the whole completion, possibly multiple tokens
             # for each completion, while there are more tokens, get more outputs
@@ -170,11 +171,11 @@ class QuirkyDataset(ABC):
                     )
                     cache = choice_outputs.past_key_values
                     # add the logit for the next token
-                    logits[j] += choice_outputs.logits[0, -1, ctoks[k + 1]]
+                    logprobs[j] += choice_outputs.logits.log_softmax(dim=-1)[0, -1, ctoks[k + 1]]
 
         # softmax adds constant to both, which cancels out, so is unnecessary here
         # log(p / (1 - p)) = log(p) - log(1 - p)
-        log_odds = logits[1] - logits[0]
+        log_odds = logprobs[1] - logprobs[0]
 
         return log_odds
 