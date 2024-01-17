79c44838ced00fd1b4f4e5031b7bc6473f342cbe
Alex Mallen <atmallen8@gmail.com>
2024-01-17 02:43:35 +0000

    many models experiments

--- elk_generalization/elk/run_transfers.py
+++ elk_generalization/elk/run_transfers.py
@@ -1,4 +1,6 @@
+import sys
 import os
+import subprocess
 
 dataset_abbrevs = {
     "all": "",
@@ -12,47 +14,45 @@ dataset_abbrevs = {
 reverse_dataset_abbrevs = {v: k for k, v in dataset_abbrevs.items()}
 
 models = [
-    # "EleutherAI/pythia-410m",
+    "EleutherAI/pythia-410m",
     # "EleutherAI/pythia-1b",
     # "EleutherAI/pythia-1.4b",
     # "EleutherAI/pythia-2.8b",
     # "EleutherAI/pythia-6.9b",
     # "EleutherAI/pythia-12b",
-    "mistralai/Mistral-7B-v0.1",
     # "meta/Llama-2-7b-hf",
+    # "mistralai/Mistral-7B-v0.1",
 ]
 user = "atmallen"
 ds_names = [
-    # "capitals",
-    # "hemisphere",
-    # "population",
-    # "sciq",
-    "sentiment",
-    # "nli",
-    # "authors",
-    # "bookrating",
-    # "addition_increment0",
-    # "subtraction_increment0",
-    # "multiplication_increment0",
-    # "modularaddition_increment0",
-    # "squaring_increment0",
+    "capitals",
+    "hemisphere",
+    "population",
+    "sciq",
+    # "sentiment",
+    "nli",
+    "authors",
+    "addition_increment0",
+    "subtraction_increment0",
+    "multiplication_increment0",
+    "modularaddition_increment0",
+    "squaring_increment0",
 ]
-weak_only = True
+weak_only = False
 
 def get_dataset_name(ds_name, abbrev, template=""):
     return f"atmallen/quirky_{ds_name}_{dataset_abbrevs[abbrev]}{template}".strip("_")
 
 
 if __name__ == "__main__":
-    exps = {
-        # "lr": ["A->A,B,AH,BH", "B->B,A", "AE->AE,AH,BH"],
-        # "mean-diff": ["A->A,B,AH,BH", "B->B,A", "AE->AE,AH,BH"],
-        "mean-diff": ["B->B","BE->B"]
-        # "lda": ["A->A,B,AH,BH", "B->B,A", "AE->AE,AH,BH"],
-        # "lr-on-pair": ["A->A,B,AH,BH", "B->B,A", "AE->AE,AH,BH"],
-        # "ccs": ["A->A,B,AH,BH", "B->B,A", "AE->AE,AH,BH", "all->all,BH"],
-        # "crc": ["A->A,B,AH,BH", "B->B,A", "AE->AE,AH,BH", "all->all,BH"],
-        # "random": ["AE->AE,BH"],
+    exps = {"mean-diff": ["B->B","BE->B"]} if weak_only else {
+        "lr": ["A->A,B,AH,BH", "B->B,A", "AE->AE,AH,BH"],
+        "mean-diff": ["A->A,B,AH,BH", "B->B,A", "AE->AE,AH,BH"],
+        "lda": ["A->A,B,AH,BH", "B->B,A", "AE->AE,AH,BH"],
+        "lr-on-pair": ["A->A,B,AH,BH", "B->B,A", "AE->AE,AH,BH"],
+        "ccs": ["A->A,B,AH,BH", "B->B,A", "AE->AE,AH,BH", "all->all,BH"],
+        "crc": ["A->A,B,AH,BH", "B->B,A", "AE->AE,AH,BH", "all->all,BH"],
+        "random": ["AE->AE,BH"],
     }
     experiments_dir = "../../experiments"
     os.makedirs(experiments_dir, exist_ok=True)
@@ -74,39 +74,75 @@ if __name__ == "__main__":
 
                 def run_extract(abbrev, ds, split, max_examples):
                     save_dir = f"{experiments_dir}/{quirky_model_last}/{abbrev}"
-                    command = (
-                        f"python extract_hiddens.py "
-                        f"--model {quirky_model} "
-                        f"--dataset {ds} "
-                        f"--save-path {save_dir} "
-                        f"--max-examples {max_examples} "
-                        f"--splits {split}"
-                    )
-                    print(command)
-                    os.system(command)
+                    # command = (
+                    #     f"python extract_hiddens.py "
+                    #     f"--model {quirky_model} "
+                    #     f"--dataset {ds} "
+                    #     f"--save-path {save_dir} "
+                    #     f"--max-examples {max_examples} "
+                    #     f"--splits {split}"
+                    # )
+                    # print(command)
+                    # os.system(command)
+                    args = [
+                        sys.executable,
+                        os.path.join(os.path.dirname(__file__), "extract_hiddens.py"),
+                        "--model",
+                        quirky_model,
+                        "--dataset",
+                        ds,
+                        "--save-path",
+                        save_dir,
+                        "--max-examples",
+                        str(max_examples),
+                        "--splits",
+                        split,
+                    ]
+                    print(f"Running {' '.join(args)}")
+                    subprocess.run(args)
 
                 run_extract(train, train_dataset, "validation", 4000)
                 for ds, abbrev in zip(test_datasets, tests):
                     run_extract(abbrev, ds, "test", 1000)
 
-                command = (
-                    f"python transfer.py --train-dir"
-                    f" {experiments_dir}/{quirky_model_last}/{train}/validation"
-                    " --test-dirs "
-                    + " ".join(
-                        [
-                            f"{experiments_dir}/{quirky_model_last}/{test}/test"
-                            for test in tests
-                        ]
-                    )
-                    + f" --reporter {reporter} --verbose "
-                )
+                # command = (
+                #     f"python transfer.py --train-dir"
+                #     f" {experiments_dir}/{quirky_model_last}/{train}/validation"
+                #     " --test-dirs "
+                #     + " ".join(
+                #         [
+                #             f"{experiments_dir}/{quirky_model_last}/{test}/test"
+                #             for test in tests
+                #         ]
+                #     )
+                #     + f" --reporter {reporter} --verbose "
+                # )
+                # if (reporter in {"ccs", "crc"} and train == "all") or (
+                #     reporter == "random" and "B" not in train
+                # ) or weak_only:
+                #     command += "--label-col alice_labels "
+                # print(command)
+                # os.system(command)
+                args = [
+                    sys.executable,
+                    os.path.join(os.path.dirname(__file__), "transfer.py"),
+                    "--train-dir",
+                    f"{experiments_dir}/{quirky_model_last}/{train}/validation",
+                    "--test-dirs",
+                ] + [
+                    f"{experiments_dir}/{quirky_model_last}/{test}/test"
+                    for test in tests
+                ] + [
+                    "--reporter",
+                    reporter,
+                    "--verbose",
+                ]
                 if (reporter in {"ccs", "crc"} and train == "all") or (
                     reporter == "random" and "B" not in train
                 ) or weak_only:
-                    command += "--label-col alice_labels "
-                print(command)
-                os.system(command)
+                    args += ["--label-col", "alice_labels"]
+                print(f"Running {' '.join(args)}")
+                subprocess.run(args)
 
             for reporter in exps:
                 for exp in exps[reporter]:
