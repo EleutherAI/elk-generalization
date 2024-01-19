e7e468eac48f9dd4da0527892b53755c166d38ee
Alex Mallen <atmallen8@gmail.com>
2024-01-19 07:38:22 +0000

    all small models and datasets

--- elk_generalization/elk/run_transfers.py
+++ elk_generalization/elk/run_transfers.py
@@ -2,6 +2,15 @@ import sys
 import os
 import subprocess
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--rank", type=int, default=0)
+
+args = parser.parse_args()
+env = dict(os.environ)
+env["CUDA_VISIBLE_DEVICES"] = str(args.rank)
+
 dataset_abbrevs = {
     "all": "",
     "A": "alice_",
@@ -15,13 +24,13 @@ reverse_dataset_abbrevs = {v: k for k, v in dataset_abbrevs.items()}
 
 models = [
     "EleutherAI/pythia-410m",
-    # "EleutherAI/pythia-1b",
-    # "EleutherAI/pythia-1.4b",
-    # "EleutherAI/pythia-2.8b",
-    # "EleutherAI/pythia-6.9b",
-    # "EleutherAI/pythia-12b",
-    # "meta/Llama-2-7b-hf",
-    # "mistralai/Mistral-7B-v0.1",
+    "EleutherAI/pythia-1b",
+    "EleutherAI/pythia-1.4b",
+    "EleutherAI/pythia-2.8b",
+    "EleutherAI/pythia-6.9b",
+    "EleutherAI/pythia-12b",
+    "meta/Llama-2-7b-hf",
+    "mistralai/Mistral-7B-v0.1",
 ]
 user = "atmallen"
 ds_names = [
@@ -29,7 +38,7 @@ ds_names = [
     "hemisphere",
     "population",
     "sciq",
-    # "sentiment",
+    "sentiment",
     "nli",
     "authors",
     "addition_increment0",
@@ -39,6 +48,9 @@ ds_names = [
     "squaring_increment0",
 ]
 weak_only = False
+
+# code to modify models and datasets based on rank
+print(ds_names, models)
 
 def get_dataset_name(ds_name, abbrev, template=""):
     return f"atmallen/quirky_{ds_name}_{dataset_abbrevs[abbrev]}{template}".strip("_")
@@ -74,16 +86,7 @@ if __name__ == "__main__":
 
                 def run_extract(abbrev, ds, split, max_examples):
                     save_dir = f"{experiments_dir}/{quirky_model_last}/{abbrev}"
-                    # command = (
-                    #     f"python extract_hiddens.py "
-                    #     f"--model {quirky_model} "
-                    #     f"--dataset {ds} "
-                    #     f"--save-path {save_dir} "
-                    #     f"--max-examples {max_examples} "
-                    #     f"--splits {split}"
-                    # )
-                    # print(command)
-                    # os.system(command)
+
                     args = [
                         sys.executable,
                         os.path.join(os.path.dirname(__file__), "extract_hiddens.py"),
@@ -99,30 +102,12 @@ if __name__ == "__main__":
                         split,
                     ]
                     print(f"Running {' '.join(args)}")
-                    subprocess.run(args)
+                    subprocess.run(args, env=env)
 
                 run_extract(train, train_dataset, "validation", 4000)
                 for ds, abbrev in zip(test_datasets, tests):
                     run_extract(abbrev, ds, "test", 1000)
 
-                # command = (
-                #     f"python transfer.py --train-dir"
-                #     f" {experiments_dir}/{quirky_model_last}/{train}/validation"
-                #     " --test-dirs "
-                #     + " ".join(
-                #         [
-                #             f"{experiments_dir}/{quirky_model_last}/{test}/test"
-                #             for test in tests
-                #         ]
-                #     )
-                #     + f" --reporter {reporter} --verbose "
-                # )
-                # if (reporter in {"ccs", "crc"} and train == "all") or (
-                #     reporter == "random" and "B" not in train
-                # ) or weak_only:
-                #     command += "--label-col alice_labels "
-                # print(command)
-                # os.system(command)
                 args = [
                     sys.executable,
                     os.path.join(os.path.dirname(__file__), "transfer.py"),
@@ -142,7 +127,7 @@ if __name__ == "__main__":
                 ) or weak_only:
                     args += ["--label-col", "alice_labels"]
                 print(f"Running {' '.join(args)}")
-                subprocess.run(args)
+                subprocess.run(args, env=env)
 
             for reporter in exps:
                 for exp in exps[reporter]:
