44f3a530ac07e3db55f731959a63420541585a0a
Alex Mallen <atmallen8@gmail.com>
2024-02-26 19:26:48 +0000

    get around requiring eval steps and save steps to match

--- elk_generalization/training/run_sft.py
+++ elk_generalization/training/run_sft.py
@@ -44,7 +44,7 @@ num_epochs = 15.0 * epoch_multiplier1 * epoch_multiplier2
 
 accum_steps = 32 // batch_size
 
-if ds_name in {"sentiment", "authors"}:
+if ds_name in {"sentiment", "authors", "nli"}:
     batch_size //= 8
     accum_steps *= 8
 if ds_name in {"sciq"}:
